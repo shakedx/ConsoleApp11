2b5272cf84353be50d5d9fde0194edc47843ec9a
shakedx <kolesnikov_danil123@mail.ru>
2021-11-25 16:57:02 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите количество минут:");
+                int t = int.Parse(Console.ReadLine());
+                int n = 1;
+                int R;
+                do
+                {
+                    R = n * 2;
+                    n++;
+                    Console.WriteLine(R);
+                }
+                while (n != t);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
